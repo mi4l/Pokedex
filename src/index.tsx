1fda05f67bbe7da5b7a1cdd5203a6129ad47ddbf
Mike Skalandunas <mskalandunas@gmail.com>
2024-03-24 22:14:07 -0700

    [CORE-63]: add working router

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,31 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 
-const App = () => <h1>Hello world</h1>;
+const Search = () => <h1>Search</h1>;
 
+const Identify = () => <h1>Identify</h1>;
 
+const App = () => {
+  return (
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/search">Search</Link>
+          </li>
+          <li>
+            <Link to="/identify">Identify</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" Component={Search} />
+        <Route path="/search" Component={Search} />
+        <Route path="/identify" Component={Identify} />
+      </Routes>
+    </Router>
+  );
+};
 
 createRoot(document.getElementById('HelloWorld') as Element).render(<App />);
